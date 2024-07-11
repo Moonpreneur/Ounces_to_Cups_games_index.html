058938a6b2fbdcb1dc390b1e615552e4720c43d8
Moonpreneur <harsh.r@moonpreneur.com>
2024-07-11 21:32:59 +0530

    Add files via upload

---
+++ question.js
@@ -0,0 +1,13 @@
+const questions = [
+    { targetCups: '2 cups'},
+    { targetCups: '3 cups'},
+    { targetCups: '4 cups'},
+    { targetCups: '5 cups'},
+    { targetCups: '6 cups'},
+    { targetCups: '7 cups'},
+    { targetCups: '8 cups'},
+    { targetCups: '9 cups'},
+    { targetCups: '10 cups'},
+];
+
+const values = [8, 16, 24, 32, 40];
